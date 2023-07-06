d8df1cd5f4ac7ec36db0ecedac6083fd02e06a31
laila1986 <lapuntel@gmail.com>
2023-07-06 10:39:05 -0500

    changing variables and re doing plots

--- code/data_processing/IL current emissions and seq.R
+++ code/data_processing/IL current emissions and seq.R
@@ -62,23 +62,33 @@ unw <- read.csv("data/unw.csv")
 # ch4 tonne C/ha Calendar year daily sum (Jan. 1 - Dec. 31) of methane emissions in co2 equivalents.
 # cseq == dsoc tonne C/ha Calendar year change in soil organic carbon stocks (Dec. 31 - Jan. 1).
 
+#insert the Net inside the bars
+
+library(tibble)
+library(ggpmisc)
+
+#data.tb <- tibble(x = "n" , y = -2.7, 
+ #                 plot = list(p +
+  #                              theme_bw(4)))
+
 
 scenil %>% filter (scenario.code == 1 & year ==2022) %>% #taking only scenario 1 and 2022 for analysis
-  select(climate_scenario, dsoc, n2o, ch4) %>%
-  gather(var, value, dsoc:ch4) %>%
-  group_by(climate_scenario, var) %>%
+  select(ghg_dsoc,  ghg_n2o, ghg_total_n2o) %>%
+  gather(var, value, ghg_dsoc:ghg_total_n2o) %>%
+  group_by (var) %>% #we decided to do average between the two climates
   summarise_all(list(mean, sd)) %>% #fn1, fn2
-  ggplot(aes(x=climate_scenario, y=fn1, group=climate_scenario)) + 
-  geom_bar(stat="identity", position=position_dodge(), aes(fill=climate_scenario)) +
+  ggplot(aes(x=var, y=fn1, fill= var)) + 
+  geom_bar(stat="identity", position=position_dodge()) +
   geom_errorbar(aes(ymin=fn1-fn2, ymax=fn1+fn2), linewidth=0.8, color="#000000",
-                position=position_dodge(0.9), width=0.2) +
-  facet_wrap(~var, scales = "free_y") +
-  scale_fill_manual(values=c("#DDAA33", "#228833", "#66CCEE"), 
+                position=position_dodge(0.9), width=0.2) + ylim(-2.7,1.5) +
+ scale_fill_manual(values=c("#DDAA33", "#228833", "#66CCEE")) + 
                     #labels=c("Corn", "Soybeans", "Winter Wheat"),
-                    name="Climate Scenario") +
+                   #name="Climate Scenario") +
   #scale_y_continuous(breaks=seq(0,70,10)) +
-  ylab("co2 equivalents (Tn/ha)") +
-  xlab("") +
+  ylab("Co2 equivalents (Tn/ha/Yr)") +
+  scale_x_discrete(labels=c("ghg_dsoc" = "SOC", "ghg_n2o" = "N2O","ghg_total_n2o" = "Total_N2O"))+
+  xlab("Greenhouse gas") +
+  #geom_plot(data = data.tb, aes(x, y, label = plot)) +
   theme_bw()+
   theme(
     panel.grid.minor=element_blank(), 
@@ -91,32 +101,84 @@ scenil %>% filter (scenario.code == 1 & year ==2022) %>% #taking only scenario 1
     strip.text=element_text(size=12, face="bold"),
     legend.text=element_text(size=11),
     legend.title=element_text(size=12, face="bold"),
-    plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")
-  )
-
-ggsave("plots/ch4_n20_dsoc_2022sn1.png", width=12, height=6)     
+    plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"), 
+    legend.position = "none")
   
+
+ggsave("plots/n20_dsoc_2022sn1.png", width=8, height=6)     
+
+#stacked with net balance start==mean
+
+#Calculate the balance
+scenil %>% filter (scenario.code == 1 & year ==2022) %>% #taking only scenario 1 and 2022 for analysis
+  select(ghg_dsoc, ghg_total_n2o) %>%
+  mutate (Net = ghg_dsoc + ghg_total_n2o) %>%
+  select (Net) %>%
+  #we decided to do average between the two climates
+  summarise_all(list(mean, sd)) %>% 
+  mutate (year = "2022") -> Net
+
+#stacked bar with Net balance
+scenil %>% filter (scenario.code == 1 & year ==2022) %>% #taking only scenario 1 and 2022 for analysis
+  select(ghg_dsoc, ghg_total_n2o) %>%
+  gather(var, value, ghg_dsoc:ghg_total_n2o) %>%
+  group_by (var) %>% #we decided to do average between the two climates
+  summarise_all(list(mean, sd)) %>%
+  mutate (year= "2022") %>% #fn1, fn2
+  ggplot(aes(x=year, y=fn1, fill= var)) + 
+  geom_bar(aes(x=year, y=fn1, fill= var), stat="identity", position= "stack") +
+  geom_errorbar(aes(ymin=fn1-fn2, ymax=fn1+fn2), linewidth=0.8, color="#000000",
+                width=0.2) + ylim(-2.7,1) +
+  scale_fill_manual(values=c("#DDAA33", "#228833"), 
+  labels=c("SOC","N2O"),
+  name="Greenhouse gas") +
+  geom_hline(yintercept = Net$fn1, linetype = "dashed", size = 1) +
+  annotate("text", x=Net$year, y=Net$fn1 +0.1, label= "Net Balance") +
+  #scale_y_continuous(breaks=seq(0,70,10)) +
+  ylab("Co2 equivalents (Tn/ha/Yr)") +
+  scale_x_discrete(labels=c("2022" = ""))+
+  xlab("Year:2022") +
+  theme_bw()+
+  theme(
+    panel.grid.minor=element_blank(), 
+    panel.grid.major=element_blank() ,
+    axis.text=element_text(size=12),
+    axis.title.x=element_text(size=12, face="bold"),
+    axis.title.y=element_text(size=12, face="bold"),
+    panel.background = element_rect(fill = 'white') ,
+    panel.border=element_rect(color="grey50", fill=NA, linewidth=0.5),
+    strip.text=element_text(size=12, face="bold"),
+    legend.text=element_text(size=11),
+    legend.title=element_text(size=12, face="bold"),
+    plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) ->p
+    #legend.position = "none")
+  
+
+ggsave("plots/Net_2022sn1.png", width=6, height=8)  
+
 
 ##by sub-scenario
 ##year by year trends 
 unw %>% filter (site_name == "IL-n_1" & year %in% (2010:2022) & climate_scenario == "rcp26") %>%  #taking only scenario 1, rcp26 and years 2010-2022 for analysis
-  select(management, year, dsoc, n2o, ch4) %>%
-  gather(var, value, dsoc:ch4) %>%
+  select(management, year, ghg_dsoc, ghg_total_n2o) %>% 
+  mutate(Net = ghg_dsoc + ghg_total_n2o) %>%
+  gather(var, value, ghg_dsoc:Net) %>%
   ggplot(aes(as.numeric(year), value, color= management)) +
   geom_point () +
   facet_wrap(~var, scales = "free_y") +
   geom_line() +
   scale_x_continuous(breaks=seq(2010,2022,1))
   
-ggsave("plots/temp1.png", width=12, height=6)     
+ggsave("plots/trend_s1_shms.png", width=12, height=6)     
 
   
   
 ###summary figure by management practice
   
 unw %>% filter (site_name == "IL-n_1" & year %in% (2010:2022) & climate_scenario == "rcp26") %>%  #taking only scenario 1, rcp26 and years 2010-2022 for analysis
-  select(management,dsoc, n2o, ch4) %>%
-  gather(var, value, dsoc:ch4) %>%
+  select(management,ghg_dsoc, ghg_total_n2o) %>%
+  mutate(Net = ghg_dsoc + ghg_total_n2o) %>%
+  gather(var, value, ghg_dsoc:Net) %>%
   group_by(management, var) %>%
   summarise_all(list(mean, sd)) %>% #fn1, fn2
   ggplot(aes(x=management, y=fn1, group=management)) + 
@@ -127,7 +189,9 @@ unw %>% filter (site_name == "IL-n_1" & year %in% (2010:2022) & climate_scenario
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 
-ggsave("plots/temp2.png", width=12, height=6)     
+ggsave("plots/mean_s1_shms.png", width=12, height=6)     
+
+#summarizing some numbers 
 
 ##do anova analysis for management effect 
 
