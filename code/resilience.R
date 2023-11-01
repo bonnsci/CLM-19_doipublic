7c24eac5981355ccd3a5cac0980f936981076d3c
bonnsci <bonniemcgill@gmail.com>
2023-11-01 17:21:42 -0400

    wrote scripts to make histogram or density plots of crop grain biomass by SPEI dry/wet category years. Made 2 plots.

--- code/resilience.R
+++ code/resilience.R
@@ -10,6 +10,8 @@
 # load packages
 library(tidyverse)
 library(MASS)  # for boxcox
+install.packages("ggh4x") 
+library(ggh4x) # for strip_themed() and facet_grid2() in ggplot
 
 # pull in data
 # biomass 
@@ -80,19 +82,139 @@ ggplot(data=speibm[speibm$crop_name %in% c("soybean") & speibm$month =="Jul",],
 
 
 
+# following fig. 5c in Scheffer et al 2015 - inferring probabilistic resilience from massive data.
+# Not sure we have "massive" data but probably enough to try this approach.
+# I'm thinking histograms of crop yields at different drought and wetness categories
+# across our "treatments".
 
-speijuly <- spei[spei$month=="Jul",]
-
-hist(speijuly$fit.12mo)
-hist(bmil$Grain.C.kgC.ha.) # bm il data contain a lot of zeros for rye grain since it is not harvested as grain
-unique(bmil$crop_name)
-bmilcs <- bmil[bmil$crop_name %in% c("corn, grain", "soybean"),]
-hist(bmilcs$Grain.C.kgC.ha.) # that's ugly
-boxcox(lm(bmilcs$Grain.C.kgC.ha.~1))
-# close to zero. Let's try the log
-hist(log(bmilcs$Grain.C.kgC.ha.)) # not much better
-hist(sqrt(bmilcs$Grain.C.kgC.ha.)) # maybe better
-boxcox(lm(sqrt(bmilcs$Grain.C.kgC.ha.)~1))
+# need to add ID variable for SPEI category for each row of data
+# again classifications from: https://droughtmonitor.unl.edu/About/AbouttheData/DroughtClassification.aspx
 
 
-hist(bmil$grainz)
+speibm$spcat1mo <- ifelse(speibm$fit.1mo >= -0.4999999 & speibm$fit.1mo <=0.49999999, "Normal",
+                          # drought conditions
+                          ifelse(speibm$fit.1mo >= -0.7999999 & speibm$fit.1mo <= -0.50, "Abnormally Dry",  
+                                 ifelse(speibm$fit.1mo >= -1.2999999 & speibm$fit.1mo <= -0.80, "Moderate Drought",
+                                        ifelse(speibm$fit.1mo >= -1.59999999 & speibm$fit.1mo <= -1.30, "Severe Drought",
+                                               ifelse(speibm$fit.1mo >= -1.9999999 & speibm$fit.1mo <= -1.60, "Extreme Drought",
+                                                      ifelse(speibm$fit.1mo <= -2.0, "Exceptional Drought",
+                           # wet conditions                           
+                                            ifelse(speibm$fit.1mo <= 0.7999999 & speibm$fit.1mo >= 0.50, "Abnormally Wet",    
+                                                  ifelse(speibm$fit.1mo <= 1.2999999 & speibm$fit.1mo >= 0.80, "Moderately Wet",
+                                                        ifelse(speibm$fit.1mo <= 1.5999999 & speibm$fit.1mo >= 1.30, "Severely Wet",
+                                                              ifelse(speibm$fit.1mo <= 1.999999 & speibm$fit.1mo >= 1.60, "Extremely Wet",
+                                                                    ifelse(speibm$fit.1mo >= 2.0, "Exceptionally Wet", "X")))))))))))
+                                                             
+# Test
+unique(speibm$spcat1mo)
+
+speibm$spcat12mo <- ifelse(speibm$fit.12mo >= -0.4999999 & speibm$fit.12mo <=0.49999999, "Normal",
+                          # drought conditions
+                          ifelse(speibm$fit.12mo >= -0.7999999 & speibm$fit.12mo <= -0.50, "Abnormally Dry",  
+                                 ifelse(speibm$fit.12mo >= -1.2999999 & speibm$fit.12mo <= -0.80, "Moderate Drought",
+                                        ifelse(speibm$fit.12mo >= -1.59999999 & speibm$fit.12mo <= -1.30, "Severe Drought",
+                                               ifelse(speibm$fit.12mo >= -1.9999999 & speibm$fit.12mo <= -1.60, "Extreme Drought",
+                                                      ifelse(speibm$fit.12mo <= -2.0, "Exceptional Drought",
+                                                             # wet conditions                           
+                                                             ifelse(speibm$fit.12mo <= 0.7999999 & speibm$fit.12mo >= 0.50, "Abnormally Wet",    
+                                                                    ifelse(speibm$fit.12mo <= 1.2999999 & speibm$fit.12mo >= 0.80, "Moderately Wet",
+                                                                           ifelse(speibm$fit.12mo <= 1.5999999 & speibm$fit.12mo >= 1.30, "Severely Wet",
+                                                                                  ifelse(speibm$fit.12mo <= 1.999999 & speibm$fit.12mo >= 1.60, "Extremely Wet",
+                                                                                         ifelse(speibm$fit.12mo >= 2.0, "Exceptionally Wet", "X")))))))))))
+
+# Test
+unique(speibm$spcat12mo)
+# NA values expected because first 11 months fit.12mo = NA. 
+
+speibm$spcat12mo <- factor(speibm$spcat12mo,
+           levels=c("Exceptional Drought","Extreme Drought","Severe Drought","Moderate Drought", "Abnormally Dry",
+                    "Normal", "Abnormally Wet", "Moderately Wet", "Severely Wet", "Extremely Wet", "Exceptionally Wet"),
+           ordered=T)
+
+
+
+# Find cutoff values for plots
+# Fig. 5C caption says probability of finding tree cover > 60% is low/high, etc. Is 60% meaningful? What is meaningful here?
+# Perhaps median grainc in spei=normal in 2020s? 75 %ile, i.e., "better than average".
+
+speibm$graincMg <- speibm$grainc/1000  # doing this makes the % frequency more like 10% rather than 0.01%
+
+norm30corn <- speibm[speibm$year>2022 & speibm$year<2030 & 
+                       speibm$spcat12mo =="Normal" &
+                       speibm$crop_name == "corn, grain",]
+
+norm30grainc_corn75p <- quantile(norm30corn$graincMg, prob=0.75)
+norm30grainc_corn90p <- quantile(norm30corn$graincMg, prob=0.90)
+norm30grainc_cornmed <- median(norm30corn$graincMg)
+
+norm30soy<- speibm[speibm$year>2022 & speibm$year<2030 & 
+                       speibm$spcat12mo =="Normal" &
+                       speibm$crop_name == "soybean",]
+
+norm30grainc_soy75p <- quantile(norm30soy$graincMg, prob=0.75)
+norm30grainc_soy90p <- quantile(norm30soy$graincMg, prob=0.90)
+norm30grainc_soymed <- median(norm30soy$graincMg)
+
+
+
+
+
+# make plots like Fig. 5c
+
+# A histogram is frequency. but a relative frequency AKA density gives % probability rather than just count.
+# if N is large enough, then relative freq histogram starts to resemble the populations distribution,
+# the area under the histogram =1, and related to probability.
+# probability is the fraction of the area under the frequency distribution curve / density curve.
+
+windows(xpinch=200, ypinch=200, width=5, height=5)
+
+
+# set facet background color per spcat
+pal11 <- c("#a50026", "#dd3d2d","#f67e4b","#FDb366","#FEDa8B", "#eaeccc", "#C2e4ef", "#98cae1","#6ea6cd", "#4a7bb7", "#364b9a")
+strip <- strip_themed(background_y = elem_list_rect(fill = pal11),
+                      text_y = elem_list_text(color=c("white", rep("black", 9), "white")))
+
+ggplot(data=speibm[speibm$crop_name=="corn, grain" & 
+                     !is.na(speibm$spcat12mo) &  # for now just compare extremes: CT+NC and NT+CC
+                     speibm$management_name %in% c("ct-nc-cn", "ct-nc-sn","nt-cc-cn", "nt-cc-sn"),],
+       aes(x=graincMg)) +
+  # geom_histogram() + # histogram (counts)
+  geom_density(aes(y=after_stat(density), # sets the y-axis units
+                   group=interaction(spcat12mo, cc, till)),
+               trim=T) +   # make sure calculating the density per facet, not whole dataset at once
+  # labs(x="Corn grain biomass C (kg/ha)", y="Frequency (count) 2022 to 2072") +
+  labs(x="Corn grain biomass C (Mg/ha)", y="Frequency (%) 2022 to 2072") +
+  scale_y_continuous(labels=scales::percent_format()) +
+  geom_vline(xintercept=norm30grainc_cornmed, color="#BBcc33") +  # 4000 chosen arbitrarily - is there something more meaningful?
+  geom_vline(xintercept=norm30grainc_corn90p, color="#44bb99") + 
+  facet_grid2(rows=vars(spcat12mo), cols=vars(cc, till), scales="free_y", 
+             labeller = as_labeller(
+               # c("Exceptional Drought"= "D4","Extreme Drought" = "D3","Severe Drought" = "D2","Moderate Drought" = "D1", "Abnormally Dry" = "D0",
+               #   "Normal" = "Normal", "Abnormally Wet" = "W0", "Moderately Wet" = "W1", "Severely Wet" = "W2", "Extremely Wet" = "W3", "Exceptionally Wet" = "W4")))
+              c("Exceptional Drought"= "Exceptional\nDrought","Extreme Drought" = "Extreme\nDrought",
+                "Severe Drought" = "Severe\nDrought","Moderate Drought" = "Moderate\nDrought", 
+                "Abnormally Dry" = "Abnormally\nDry", "Normal" = "Normal", 
+                "Abnormally Wet" = "Abnormally\nWet", "Moderately Wet" = "Moderately\nWet", 
+                "Severely Wet" = "Severely\nWet", "Extremely Wet" = "Extremely\nWet", 
+                "Exceptionally Wet" = "Exceptionally\nWet",
+                "CC" = "With cover crops", "NC"= "Without cover crops",
+                "NT" = "No-till", "CT" = "Conventional till")),
+              strip = strip_themed(background_y = elem_list_rect(fill = pal11),  # cool stuff from ggh4x package!
+                                   text_y = elem_list_text(color=c("white", rep("black", 9), "white")))) +
+  theme(
+    panel.grid.minor=element_blank(), 
+    panel.grid.major=element_blank(),  #,
+    panel.background = element_rect(fill = 'gray96')
+  )	 
+
+# ggsave("plots/resilience/IL_corngrainc_bydrought_hist.png", width=4, height=10, dpi=300)
+ggsave("plots/resilience/IL_corngrainc_bydrought_density.png", width=4, height=10, dpi=300)
+
+# why so many NA spcat12mo values?
+# check that they are all in 2022
+
+# ch <- speibm[is.na(speibm$spcat12mo),]
+# unique(ch$year) # just 2022
+# unique(ch$month) # Jan. - Nov. as expected
+# sum(is.na(ch[ch$year==2022,"fit.12mo"]))  # 19008
+# sum(is.na(ch[!ch$year==2022,"fit.12mo"]))  # 0
