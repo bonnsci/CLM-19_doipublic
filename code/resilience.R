f8e96059fd49bfd00440add1fb1b441ab9271fbf
BonnSci <bonniemcgill@gmail.com>
2023-11-06 16:51:39 -0500

    measured area under PDF curve, comparing areas between treatments*SPEI categories.

--- code/resilience.R
+++ code/resilience.R
@@ -10,8 +10,9 @@
 # load packages
 library(tidyverse)
 library(MASS)  # for boxcox
-install.packages("ggh4x") 
+# install.packages("ggh4x") 
 library(ggh4x) # for strip_themed() and facet_grid2() in ggplot
+
 
 # pull in data
 # biomass 
@@ -80,7 +81,14 @@ ggplot(data=speibm[speibm$crop_name %in% c("soybean") & speibm$month =="Jul",],
   geom_point(size=0.1, alpha=0.5) +
   facet_grid(cols=vars(ns, till), rows=vars(cc))
 
+# why so many NA spcat12mo values?
+# check that they are all in 2022
 
+# ch <- speibm[is.na(speibm$spcat12mo),]
+# unique(ch$year) # just 2022
+# unique(ch$month) # Jan. - Nov. as expected
+# sum(is.na(ch[ch$year==2022,"fit.12mo"]))  # 19008
+# sum(is.na(ch[!ch$year==2022,"fit.12mo"]))  # 0
 
 # following fig. 5c in Scheffer et al 2015 - inferring probabilistic resilience from massive data.
 # Not sure we have "massive" data but probably enough to try this approach.
@@ -171,8 +179,7 @@ windows(xpinch=200, ypinch=200, width=5, height=5)
 
 # set facet background color per spcat
 pal11 <- c("#a50026", "#dd3d2d","#f67e4b","#FDb366","#FEDa8B", "#eaeccc", "#C2e4ef", "#98cae1","#6ea6cd", "#4a7bb7", "#364b9a")
-strip <- strip_themed(background_y = elem_list_rect(fill = pal11),
-                      text_y = elem_list_text(color=c("white", rep("black", 9), "white")))
+
 
 ggplot(data=speibm[speibm$crop_name=="corn, grain" & 
                      !is.na(speibm$spcat12mo) &  # for now just compare extremes: CT+NC and NT+CC
@@ -210,11 +217,142 @@ ggplot(data=speibm[speibm$crop_name=="corn, grain" &
 # ggsave("plots/resilience/IL_corngrainc_bydrought_hist.png", width=4, height=10, dpi=300)
 ggsave("plots/resilience/IL_corngrainc_bydrought_density.png", width=4, height=10, dpi=300)
 
-# why so many NA spcat12mo values?
-# check that they are all in 2022
 
-# ch <- speibm[is.na(speibm$spcat12mo),]
-# unique(ch$year) # just 2022
-# unique(ch$month) # Jan. - Nov. as expected
-# sum(is.na(ch[ch$year==2022,"fit.12mo"]))  # 19008
-# sum(is.na(ch[!ch$year==2022,"fit.12mo"]))  # 0
+
+
+# Quantify areas under curves by site, SPEI, and treatments
+
+lsd <- speibm[speibm$crop_name %in% c("corn, grain", "soybean"),] %>%
+  group_split(site, crop_name, spcat12mo, till, cc, nfert) # by site, so that we can compare means and variability of treatments across sites
+
+
+lecdf <- lapply(lsd, function(i){
+  d_fun <- ecdf(i$graincMg)
+  cropmed <- ifelse(i$crop_name[1] == "corn, grain", norm30grainc_cornmed, norm30grainc_soymed)
+  crop90p <- ifelse(i$crop_name[1] == "corn, grain", norm30grainc_corn90p, norm30grainc_soy90p)
+  grmed <- 1 - d_fun(cropmed) # 1- gives the area under the curve to the RIGHT of the value
+  gr90p <- 1 - d_fun(crop90p)
+  out <- i[1, c("site", "crop_name", "till", "cc", "nfert", "spcat12mo")]
+  out$grmed <- grmed
+  out$gr90p <- gr90p
+  out
+})
+
+lecdf <- lecdf %>%
+  bind_rows(lecdf)
+
+rm(lsd)
+
+# are the areas under the curves different?
+
+corndf <- lecdf[lecdf$crop_name=="corn, grain" & !is.na(lecdf$spcat12mo),]
+
+cornaov <- aov(grmed~till*cc*nfert*spcat12mo, data=corndf)
+summary(cornaov)
+# Df Sum Sq Mean Sq  F value   Pr(>F)    
+# till                        2    0.7    0.36    9.070 0.000116 ***
+# cc                          1    3.4    3.43   85.803  < 2e-16 ***
+# nfert                       2  100.8   50.39 1259.084  < 2e-16 ***
+# spcat12mo                  10  288.6   28.86  721.153  < 2e-16 ***
+# till:cc                     2    0.1    0.03    0.736 0.478825    
+# till:nfert                  4    5.2    1.31   32.685  < 2e-16 ***
+# cc:nfert                    2   20.6   10.28  256.911  < 2e-16 ***
+# till:spcat12mo             20    2.1    0.10    2.575 0.000138 ***
+# cc:spcat12mo               10    0.7    0.07    1.636 0.089931 .  
+# nfert:spcat12mo            20   28.1    1.40   35.053  < 2e-16 ***
+# till:cc:nfert               4    1.5    0.36    9.076 2.57e-07 ***
+# till:cc:spcat12mo          20    0.5    0.02    0.565 0.938112    
+# till:nfert:spcat12mo       40    0.4    0.01    0.248 1.000000    
+# cc:nfert:spcat12mo         20    1.2    0.06    1.493 0.072330 .  
+# till:cc:nfert:spcat12mo    40    0.1    0.00    0.076 1.000000    
+# Residuals               12474  499.2    0.04                      
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+Tukout <- TukeyHSD(cornaov)
+
+# put interaction output into a dataframe we can sort
+Tukout <- as.data.frame(Tukout[5]) %>%  # 5 is the list item for the interaction that looks significant from the summary above
+  rownames_to_column(., "term") %>%
+  arrange(term)
+
+
+# assumptions - following https://statsandr.com/blog/anova-in-r
+
+# Independence - the observations (grain.c.kgc.ha. observations) are independent, one does
+# not depend on the other, they're not dependent on some other variable like, 
+# from one individual came multiple observations.
+
+# Normality - not required with large enough sample size >30.  But we can test anyway. 
+# the residuals (observed - mean for that group) should be normally distributed.
+qqnorm(corndf$grmed)
+qqline(corndf$grmed)
+hist(corndf$grmed-mean(corndf$grmed))
+hist(corndf$grmed)
+
+# Equality of variances 
+ggplot(data=corndf, aes(x=interaction(till, cc, spcat12mo), y=grmed)) + 
+  geom_boxplot() +
+  theme(axis.text.x=element_text(angle=45, vjust=1, hjust=1))
+# an interesting and not so pretty graph
+car::leveneTest(grmed ~ cc*till*spcat12mo, data=corndf)
+# Levene's Test for Homogeneity of Variance (center = median)
+#          Df F value    Pr(>F)    
+# group    65    15.2 < 2.2e-16 ***
+#       12606                      
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# HOWEVER: it seems that our large sample size (>35k data points) might be affecting this test
+# See this from https://www.theanalysisfactor.com/the-problem-with-tests-for-statistical-assumptions/
+# It relies too much on p-values, and therefore, sample sizes. If the sample size is large, 
+# Levene’s will have a smaller p-value than if the sample size is small, given the same variances.
+# So it’s very likely that you’re ***overstating a problem*** with the assumption in large samples and understating 
+# it in small samples. You can’t ignore the actual size difference in the variances when making this decision. 
+# So sure, look at the p-value, but also look at the actual variances and how much bigger some are than others. 
+# (In other words, actually look at the effect size, not just the p-value).
+# The ANOVA is generally considered robust to violations of this assumption when sample sizes 
+# across groups are equal. So even if Levene’s is significant, moderately different variances may not be a 
+# problem in balanced data sets. Keppel (1992) suggests that a good rule of thumb is that if sample sizes are equal, 
+# robustness should hold until the largest variance is more than 9 times the smallest variance.
+# This robustness goes away the more unbalanced the samples are. So you need to use judgment here,
+# taking into account both the imbalance and the actual difference in variances.
+# *** emphasis added
+
+# outliers
+summary(corndf$grmed)
+# > summary(corndat$Grain.C.kgC.ha.)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 0.0000  0.3636  0.5625  0.5469  0.7692  1.0000 
+
+# outliers via histograms
+hist(corndf$grmed, breaks=sqrt(nrow(corndf)))
+
+ggplot(data=corndf, aes(x=grmed)) +
+  geom_histogram() +
+  facet_grid(rows=vars(spcat12mo), cols=vars(till, cc))
+
+#outliers via boxplots
+boxplot(grmed~till*spcat12mo, data=corndf) # 
+
+# outliers via z-scores (not sure if this works for data that are already between 0-1)
+corndf$grmedz <- scale(corndf$grmed)
+hist(corndf$grmedz)
+summary(corndf$grmedz)
+# V1          
+# Min.   :-1.99416  
+# 1st Qu.:-0.66826  
+# Median : 0.05684  
+# Mean   : 0.00000  
+# 3rd Qu.: 0.81063  
+# Max.   : 1.65206
+# < -2 or >2 is considered rare
+# < -3 or >3 is extremely rare
+# < -3.29 or > -3.29 is used to detect outliers, where one out of 1000 observations will be outside
+# we're good here
+
+## outlier summary: boxplots and z-score suggest no extreme outliers. No justification for removing.
+
+# re-run ANOVA - are we accounting for the 0-1 distribution properly?
+
+########### left off here
+
